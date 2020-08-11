aff0c23d57d65577313d72cafb2beace907673f7
WGiger <bgigs37@gmail.com>
2020-08-10 19:27:16 -0500

    project working with react routing

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {NavLink} from "react-router-dom/";
+
+const Header = () => (
+  <header>
+    <span className="icn-logo"><i className="material-icons">code</i></span>
+    <ul className="main-nav">
+      <li><NavLink exact to="/" activeStyle={{background:'purple'}}>Home</NavLink></li>
+      <li><NavLink to="/about" activeStyle={{background:'blue'}}>About</NavLink></li>
+      <li><NavLink to="/teachers" activeStyle={{background:'green'}}>Teachers</NavLink></li>
+      <li><NavLink to="/courses" activeStyle={{background:'red'}}>Courses</NavLink></li>
+    </ul>    
+  </header>
+
+);
+
+export default Header;
